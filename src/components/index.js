9a8a9003436b7b8fbeaa1c38be7299d18cf85e9c
jitendra <jitendrasaini352jjnu@gmail.com>
2023-03-30 15:12:16 +0530

    created file structure for my app

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Sidebar from "./Sidebar";
+import Searchbar from "./Searchbar";
+import SongCard from "./SongCard";
+import TopPlay from "./TopPlay";
+import ArtistCard from "./ArtistCard";
+import DetailsHeader from "./DetailsHeader";
+import SongBar from "./SongBar";
+import RelatedSongs from "./RelatedSongs";
+// import MusicPlayer from "./MusicPlayer";
+// MusicPlayer,
+import Loader from "./Loader";
+import Error from "./Error";
+
+export {
+  TopPlay,
+  Sidebar,
+  SongCard,
+  Searchbar,
+  ArtistCard,
+  DetailsHeader,
+  RelatedSongs,
+  SongBar,
+  Loader,
+  Error,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Discover from "./Discover.jsx";
+import ArtistDetails from "./ArtistDetails.jsx";
+import AroundYou from "./AroundYou.jsx";
+import Search from "./Search.jsx";
+import SongDetails from "./SongDetails.jsx";
+import TopArtists from "./TopArtists.jsx";
+import TopCharts from "./TopCharts.jsx";
+
+export {
+  Discover,
+  ArtistDetails,
+  AroundYou,
+  Search,
+  SongDetails,
+  TopArtists,
+  TopCharts,
+};
