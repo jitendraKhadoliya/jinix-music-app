8a4cd173586435eb74b14a770eb20723699aa2e9
jitendra <jitendrasaini352jjnu@gmail.com>
2023-04-05 20:02:34 +0530

    created loading and error file and implementing songCard

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Error = () => <div>Error</div>;
+const Error = () => (
+  <div className="flex justify-center items-center w-full ">
+    <h1 className=" text-2xl text-white font-bold">Something Went Wrong....</h1>
+  </div>
+);
 
 export default Error;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,6 +6,8 @@ const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
   console.log("data", data);
   // console.log(genres);
+  if (isFetching) return <Loader title={"Songs Loading...."} />;
+  if (error) return <Error />;
   return (
     <div className=" flex flex-col">
       <div className=" w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -22,7 +24,7 @@ const Discover = () => {
       </div>
       {/* here I am going to create wrapper for song */}
       <div className=" flex flex-wrap sm:justify-start justify-center gap-6">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((song, i) => (
+        {data?.tracks?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
